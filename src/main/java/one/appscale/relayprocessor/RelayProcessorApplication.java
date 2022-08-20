270a27dc441519ac4ff8a7c3cac63c4e349ae2b2
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-20 21:49:27 +0900

    init

---
+++ src/main/java/one/appscale/relayprocessor/RelayProcessorApplication.java
@@ -0,0 +1,13 @@
+package one.appscale.relayprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelayProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/appscale/relayprocessor/RelayProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package one.appscale.relayprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
