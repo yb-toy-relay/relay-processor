e849e2bd1157f3a1afffa012e4316631baea315b
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-24 01:26:42 +0900

    prod mongodb connection test #7

--- src/main/java/one/appscale/relayprocessor/infra/mongodb/MongoDBConfiguration.java
+++ src/main/java/one/appscale/relayprocessor/infra/mongodb/MongoDBConfiguration.java
@@ -1,37 +1,37 @@
-//package one.appscale.relayprocessor.infra.mongodb;
-//
-//import com.mongodb.MongoClientSettings;
-//import com.mongodb.connection.ConnectionPoolSettings;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//@Configuration
-//public class MongoDBConfiguration {
-////    @Bean
-////    public MongoClientSettings mongoClientSettings() {
-////        return MongoClientSettings.builder()
-////                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
-////                                      builder.maxConnectionIdleTime(10, TimeUnit.SECONDS);
-////                                  })
-////                                  .build();
-////    }
-//
-////    @Bean
-////    public MongoClientSettings mongoClientSettings() {
-////        return MongoClientSettings.builder()
-////                                  .retryWrites(true)
-////                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
-////                                      builder.maxSize(300) //connections count
-////                                             .minSize(100)
-////                                             .maxConnectionLifeTime(0, TimeUnit.MILLISECONDS)
-////                                             .maxConnectionIdleTime(0, TimeUnit.MILLISECONDS)
-////                                             .maxWaitTime(5000, TimeUnit.MILLISECONDS);
-////                                  })
-////                                  .applyToSocketSettings(builder -> {
-////                                      builder.connectTimeout(5000, TimeUnit.MILLISECONDS);
-////                                  })
-////                                  .build();
-////    }
-//}
+package one.appscale.relayprocessor.infra.mongodb;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.connection.ConnectionPoolSettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class MongoDBConfiguration {
+    @Bean
+    public MongoClientSettings mongoClientSettings() {
+        return MongoClientSettings.builder()
+                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
+                                      builder.maxConnectionIdleTime(10, TimeUnit.SECONDS);
+                                  })
+                                  .build();
+    }
+
+//    @Bean
+//    public MongoClientSettings mongoClientSettings() {
+//        return MongoClientSettings.builder()
+//                                  .retryWrites(true)
+//                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
+//                                      builder.maxSize(300) //connections count
+//                                             .minSize(100)
+//                                             .maxConnectionLifeTime(0, TimeUnit.MILLISECONDS)
+//                                             .maxConnectionIdleTime(0, TimeUnit.MILLISECONDS)
+//                                             .maxWaitTime(5000, TimeUnit.MILLISECONDS);
+//                                  })
+//                                  .applyToSocketSettings(builder -> {
+//                                      builder.connectTimeout(5000, TimeUnit.MILLISECONDS);
+//                                  })
+//                                  .build();
+//    }
+}
