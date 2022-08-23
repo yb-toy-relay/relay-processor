97aac96ba0bef40d54785bb1585e5774fe7f163b
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-24 02:49:08 +0900

    cause: Atlas access list CIDR notation problem
    - changed 172.31.0.0/16 to 172.31.0.0/0

--- src/main/java/one/appscale/relayprocessor/infra/mongodb/MongoDBConfiguration.java
+++ src/main/java/one/appscale/relayprocessor/infra/mongodb/MongoDBConfiguration.java
@@ -7,7 +7,6 @@
 //import com.mongodb.connection.ClusterSettings;
 //import com.mongodb.connection.ConnectionPoolSettings;
 //import com.mongodb.connection.SocketSettings;
-//import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //
@@ -15,54 +14,27 @@
 //
 //@Configuration
 //public class MongoDBConfiguration {
-//    @Value("${app.mongodb.password}")
-//    private String password;
-//
 //    @Bean
 //    public MongoClientSettings mongoClientSettings() {
-//        final MongoCredential credential = MongoCredential.createCredential("prod_relay_all",
-//                                                                            "relaydb",
-//                                                                            password.toCharArray());
+////        final MongoCredential credential = MongoCredential.createCredential("prod_relay_all",
+////                                                                            "relaydb",
+////                                                                            "password".toCharArray());
 //        return MongoClientSettings.builder()
 //                                  .retryWrites(true)
+//                                  .writeConcern(WriteConcern.MAJORITY)
+////                                  .credential(credential)
 //                                  .applyToClusterSettings((ClusterSettings.Builder builder) -> {
 //                                      builder.srvHost("prod-relay.jyryu.mongodb.net")
 //                                          .srvServiceName("mongodb")
 //                                          .mode(ClusterConnectionMode.LOAD_BALANCED);
 //                                  })
-//                                  .credential(credential)
+//                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
+//                                      builder.maxConnectionIdleTime(10, TimeUnit.SECONDS);
+//                                  })
+//                                  .applyToSocketSettings((SocketSettings.Builder builder) -> {
+//                                      builder.connectTimeout(10, TimeUnit.SECONDS)
+//                                             .readTimeout(10, TimeUnit.SECONDS);
+//                                  })
 //                                  .build();
 //    }
-//
-////    @Bean
-////    public MongoClientSettings mongoClientSettings() {
-////        return MongoClientSettings.builder()
-////                                  .retryWrites(true)
-////                                  .writeConcern(WriteConcern.MAJORITY)
-////                                  .applyToSocketSettings((SocketSettings.Builder builder) -> {
-////                                      builder.connectTimeout(10, TimeUnit.SECONDS)
-////                                             .readTimeout(10, TimeUnit.SECONDS);
-////                                  })
-////                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
-////                                      builder.maxConnectionIdleTime(10, TimeUnit.SECONDS);
-////                                  })
-////                                  .build();
-////    }
-//
-////    @Bean
-////    public MongoClientSettings mongoClientSettings() {
-////        return MongoClientSettings.builder()
-////                                  .retryWrites(true)
-////                                  .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> {
-////                                      builder.maxSize(300) //connections count
-////                                             .minSize(100)
-////                                             .maxConnectionLifeTime(0, TimeUnit.MILLISECONDS)
-////                                             .maxConnectionIdleTime(0, TimeUnit.MILLISECONDS)
-////                                             .maxWaitTime(5000, TimeUnit.MILLISECONDS);
-////                                  })
-////                                  .applyToSocketSettings(builder -> {
-////                                      builder.connectTimeout(5000, TimeUnit.MILLISECONDS);
-////                                  })
-////                                  .build();
-////    }
 //}
